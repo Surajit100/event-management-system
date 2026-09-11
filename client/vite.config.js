82fb00655863d8abcc7ee9a8054e9966110f8ece
Surajit Bhattacharjee <pappu.fossil.99@gmail.com>
2026-09-12 02:31:11 +0530

    Allow Railway frontend host

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  preview: {
+    allowedHosts: [
+      "respectful-flexibility-production-b12f.up.railway.app",
+    ],
+  },
 });
